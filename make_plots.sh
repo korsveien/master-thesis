1376b08a25a651a4a0748c4f079183e598707655
Nils Peder Korsveien <nilspk@ifi.uio.no>
2014-07-18 14:39:25 +0200

    Add all plots to eval chapter

---
+++ make_plots.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+gnuplot "betweenness.gpi"
+gnuplot "closeness.gpi"
+gnuplot "eccentricity.gpi"
+gnuplot "cc.gpi"
+gnuplot "degree.gpi"
+gnuplot "indegree.gpi"
+gnuplot "outdegree.gpi"
